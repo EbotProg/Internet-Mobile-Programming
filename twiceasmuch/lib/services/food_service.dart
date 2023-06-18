a21aecaa6663990e32302bbcacbc57183449040d
Randy Kwalar <rkwalar@gmail.com>
2023-06-18 17:54:27 +0100

    Add food methods

--- twiceasmuch/lib/services/food_service.dart
+++ twiceasmuch/lib/services/food_service.dart
@@ -1,183 +1,125 @@
-// import 'package:lone_soul/models/match.dart';
-// import 'package:lone_soul/models/preference.dart';
-// import 'package:lone_soul/models/user.dart';
-// import 'package:lone_soul/services/db/conversation_db_methods.dart';
-// import 'package:lone_soul/services/db/user_db_methods.dart';
-// import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:twiceasmuch/enums/food_state.dart';
+import 'package:twiceasmuch/models/food.dart';
 
-// class FoodService {
-//   final supabaseInstance = Supabase.instance;
+class FoodService {
+  final supabaseInstance = Supabase.instance;
 
-//   //insert users to database
-//   Future<List<MatchUser>> getMatches(String userId) async {
-//     final userDBMethods = UserDBMethods();
-//     final conversationDBMethods = ConversationDBMethods();
-//     try {
-//       final conversations =
-//           await conversationDBMethods.getConversations(userId);
-//       final map1 = await supabaseInstance.client
-//           .from('matches')
-//           .select<List<Map<String, dynamic>>>()
-//           .contains('user_id_1', userId);
-//       final map2 = await supabaseInstance.client
-//           .from('matches')
-//           .select<List<Map<String, dynamic>>>()
-//           .contains('user_id_2', userId);
-//       final finalMap = [...map1, ...map2];
+  //insert users to database
+  Future<List<Food>> getFoods() async {
+    try {
+      final foodMaps = await supabaseInstance.client
+          .from('food')
+          .select<List<Map<String, dynamic>>>()
+          .order('uploadedAt');
+      final foods = foodMaps.map((e) => Food.fromJson(e)).toList();
 
-//       final requests = finalMap.map((e) {
-//         if (e['user_id_1'] != userId) {
-//           return userDBMethods.getUser(e['user_id_1']);
-//         }
+      return foods;
+    } on PostgrestException catch (e) {
+      print(e);
+      return [];
+    } catch (e) {
+      print(e);
+      return [];
+    }
+  }
 
-//         return userDBMethods.getUser(e['user_id_2']);
-//       });
+  Future<List<Food>> getTrendingFoods() async {
+    try {
+      final foodMaps = await supabaseInstance.client
+          .from('food')
+          .select<List<Map<String, dynamic>>>()
+          .order('uploadedAt');
+      final foods = foodMaps.map((e) => Food.fromJson(e)).toList();
 
-//       final results = await Future.wait(requests);
+      return foods;
+    } on PostgrestException catch (e) {
+      print(e);
+      return [];
+    } catch (e) {
+      print(e);
+      return [];
+    }
+  }
 
-//       final matches = <MatchUser>[];
-//       for (int i = 0; i < results.length; i++) {
-//         if (results[i] == null ||
-//             conversations.any(
-//               (element) => element.userId == results[i]?.userId,
-//             )) {
-//           continue;
-//         }
-//         matches.add(
-//           MatchUser(
-//             id: finalMap[i]['id'],
-//             match: results[i],
-//             matchScore: finalMap[i]['match_score'],
-//           ),
-//         );
-//       }
+  Future<List<Food>> getRecommendedFoods() async {
+    try {
+      final foodMaps = await supabaseInstance.client
+          .from('food')
+          .select<List<Map<String, dynamic>>>()
+          .order('uploadedAt', ascending: true);
+      final foods = foodMaps.map((e) => Food.fromJson(e)).toList();
 
-//       return matches;
-//     } on PostgrestException catch (e) {
-//       print(e);
-//       return [];
-//     } catch (e) {
-//       print(e);
-//       return [];
-//     }
-//   }
+      return foods;
+    } on PostgrestException catch (e) {
+      print(e);
+      return [];
+    } catch (e) {
+      print(e);
+      return [];
+    }
+  }
 
-//   //insert users to database
-//   Future<List<MatchUser>> searchForMatches(
-//     String userId,
-//     Preference? preference,
-//   ) async {
-//     final conversationDBMethods = ConversationDBMethods();
-//     try {
-//       final userDBMethods = UserDBMethods();
+  Future<List<Food>> getUploadedFoods(String userId) async {
+    try {
+      final foodMaps = await supabaseInstance.client
+          .from('food')
+          .select<List<Map<String, dynamic>>>()
+          .contains('donorID', userId)
+          .order('uploadedAt');
+      final foods = foodMaps.map((e) => Food.fromJson(e)).toList();
 
-//       final user1 = await userDBMethods.getUser(userId);
+      return foods;
+    } on PostgrestException catch (e) {
+      print(e);
+      return [];
+    } catch (e) {
+      print(e);
+      return [];
+    }
+  }
 
-//       if (user1 == null) return [];
+  Future<List<Food>> searchFoods({
+    required String value,
+    required FoodState foodState,
+  }) async {
+    try {
+      final foodMaps = await supabaseInstance.client
+          .from('food')
+          .select<List<Map<String, dynamic>>>()
+          .textSearch('name', value)
+          .contains('state', foodState.toString())
+          .order('uploadedAt');
 
-//       if (preference != null) {
-//         user1.preference = preference;
-//       }
+      final foods = foodMaps.map((e) => Food.fromJson(e)).toList();
 
-//       final users = await supabaseInstance.client
-//           .from('users')
-//           .select<List<Map<String, dynamic>>>('id');
+      return foods;
+    } on PostgrestException catch (e) {
+      print(e);
+      return [];
+    } catch (e) {
+      print(e);
+      return [];
+    }
+  }
 
-//       final conversations =
-//           await conversationDBMethods.getConversations(userId);
+    Future<List<Food>> getFoodsPerIds(List<String> ids) async {
+    try {
+      final foodMaps = await supabaseInstance.client
+          .from('food')
+          .select<List<Map<String, dynamic>>>()
+          .in_('foodID', ids)
+          .order('uploadedAt');
 
-//       users.removeWhere(
-//         (user) => conversations.any(
-//           (conversation) => conversation.userId == user['id'],
-//         ),
-//       );
+      final foods = foodMaps.map((e) => Food.fromJson(e)).toList();
 
-//       final userRequests = users.map((e) {
-//         return getMatchScore(userId, e['id']);
-//       });
-
-//       final results = await Future.wait(userRequests);
-
-//       results.removeWhere((e) => e == null);
-//       final realResults = results.map((e) => e!).toList();
-//       realResults.sort((a, b) => a.matchScore.compareTo(b.matchScore));
-
-//       return realResults;
-//     } on PostgrestException catch (e) {
-//       print(e);
-//       return [];
-//     } catch (e) {
-//       print(e);
-//       return [];
-//     }
-//   }
-
-//   Future<MatchUser?> getMatchScore(String userID1, String userID2) async {
-//     try {
-//       final userDBMethods = UserDBMethods();
-
-//       final user1 = await userDBMethods.getUser(userID1);
-//       final user2 = await userDBMethods.getUser(userID2);
-
-//       if (user1 == null || user2 == null) return null;
-
-//       return MatchUser(
-//         matchScore: _checkAge(user1, user2) +
-//             _checkGender(user1, user2) +
-//             _checkInterests(user1, user2),
-//         match: user2,
-//       );
-//     } on PostgrestException catch (e) {
-//       print(e);
-//       return null;
-//     }
-//   }
-
-//   double _checkAge(AppUser user1, AppUser user2) {
-//     double score = 0;
-//     if ((user1.age ?? 0) <= (user2.preference?.maxAge ?? 0) &&
-//         (user1.age ?? 0) >= (user2.preference?.minAge ?? 0)) {
-//       score += 1;
-//     }
-
-//     if ((user2.age ?? 0) <= (user1.preference?.maxAge ?? 0) &&
-//         (user2.age ?? 0) >= (user1.preference?.minAge ?? 0)) {
-//       score += 1;
-//     }
-
-//     return score;
-//   }
-
-//   double _checkGender(AppUser user1, AppUser user2) {
-//     double score = 0;
-//     if ((user1.gender ?? 'man') == (user2.preference?.gender ?? 'man')) {
-//       score += 1;
-//     }
-
-//     if ((user2.gender ?? 'man') == (user1.preference?.gender ?? 'man')) {
-//       score += 1;
-//     }
-
-//     return score;
-//   }
-
-//   double _checkInterests(AppUser user1, AppUser user2) {
-//     double score = 0;
-
-//     final similarities1 = user1.preference?.interests
-//             ?.where((element) => user2.interests?.contains(element) ?? false) ??
-//         [];
-
-//     score +=
-//         (similarities1.length / (user1.preference?.interests?.length ?? 1)) * 3;
-
-//     final similarities2 = user2.preference?.interests
-//             ?.where((element) => user1.interests?.contains(element) ?? false) ??
-//         [];
-
-//     score +=
-//         (similarities2.length / (user2.preference?.interests?.length ?? 1)) * 3;
-
-//     return score;
-//   }
-// }
+      return foods;
+    } on PostgrestException catch (e) {
+      print(e);
+      return [];
+    } catch (e) {
+      print(e);
+      return [];
+    }
+  }
+}
